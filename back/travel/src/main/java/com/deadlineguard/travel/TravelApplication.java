4c4059401e27f96f68142a74fd333ac464ce3583
valeriyshashkin <velianjanjan@gmail.com>
2021-10-23 00:09:25 +0500

    добавил простой парсинг странички гугла

--- back/travel/src/main/java/com/deadlineguard/travel/TravelApplication.java
+++ back/travel/src/main/java/com/deadlineguard/travel/TravelApplication.java
@@ -4,14 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+import java.lang.StringBuilder;
 
 @RestController
 @SpringBootApplication
 public class TravelApplication {
 
 	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	public String index() throws java.io.IOException {
+		Document doc = Jsoup.connect("https://google.com/").get();
+
+		// точка в select означает class
+		// например, если в html есть тег где class="test"
+		// то нужно написать doc.select(".test").first()
+
+		// first нужен для взятия первого элемента, так как
+		// возвращается массив Elements
+		Element someText = doc.select(".ktLKi").first();
+
+		// должно выдать что то про выбросы углекислых газов
+		// со странички гугла
+		return someText.text();
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "test";
 	}
 
 	public static void main(String[] args) {
