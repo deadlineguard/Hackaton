f9992e801fcafe62e2d78bb8341fd2b6b311398b
valeriyshashkin <velianjanjan@gmail.com>
2021-10-22 23:44:21 +0500

    добавил тестовый ответ апи

--- back/travel/src/main/java/com/deadlineguard/travel/TravelApplication.java
+++ back/travel/src/main/java/com/deadlineguard/travel/TravelApplication.java
@@ -2,9 +2,17 @@ package com.deadlineguard.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TravelApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelApplication.class, args);
