d144db7fd7033c6a44817401cf9613f2ce0fec19
Andrew <andreyy0strijov@gmail.com>
2021-10-23 03:09:30 +0500

    добавил интерфейс (чучут)

--- front/app/src/main/java/com/example/front/MainActivity.kt
+++ front/app/src/main/java/com/example/front/MainActivity.kt
@@ -2,17 +2,48 @@ package com.example.front
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ListView
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var userTransport: EditText? = null
+    var isTrain: Boolean = false
+    var isAirplane: Boolean = false
+    var isOwnCar: Boolean = false
+    var maxPrice: Int = 1000 // нужно установить значение из парсинга
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userTransport = findViewById(R.id.userTransport)
+        val switchTrain: Switch = findViewById(R.id.switchTrain)
+        val switchAirplane: Switch = findViewById(R.id.switchAirplane)
+        val switchOwnCar: Switch = findViewById(R.id.switchOwnCar)
+        val seekPrice: SeekBar = findViewById(R.id.seekPrice)
+        val textPrice: TextView = findViewById(R.id.textPrice)
 
+        // обработка нажатий на switch
+        switchTrain.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked) isTrain = true
+            else isTrain = false
+        }
+        switchAirplane.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked) isAirplane = true
+            else isAirplane = false
+        }
+        switchOwnCar.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if(isChecked) isOwnCar = true
+            else isOwnCar = false
+        }
+
+        seekPrice.max = maxPrice // устанавливаем макс. цену
+        seekPrice.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                textPrice.setText(progress.toString()) // дередаём textview значение прогресса
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
 }
