fd7ddeae313299f13927f29d8b86d5c3d284e4af
Andrew <andreyy0strijov@gmail.com>
2021-10-23 00:31:49 +0500

    делаю, делаю, только не бейте

--- front/app/src/main/java/com/example/front/MainActivity.kt
+++ front/app/src/main/java/com/example/front/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.front
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private var userTransport: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userTransport = findViewById(R.id.userTransport)
+
     }
 }
