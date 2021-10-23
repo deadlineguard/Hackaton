b9449a580bbae117970c8214c97beb27f4ec610b
Andrew <andreyy0strijov@gmail.com>
2021-10-23 14:06:11 +0500

    something

--- back/travel/src/main/java/com/deadlineguard/travel/TravelApplication.java
+++ back/travel/src/main/java/com/deadlineguard/travel/TravelApplication.java
@@ -1,34 +1,5 @@
 package com.deadlineguard.travel;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.jsoup.nodes.Document;
-import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
-import org.jsoup.nodes.Element;
-import java.lang.StringBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.util.ArrayList;
-
-import javax.management.Query;
-
-@RestController
-@SpringBootApplication
 public class TravelApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TravelApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String indus() throws java.io.IOException{
-		Document doc = Jsoup.connect("https://www.usue.ru/").get();
-		Element alltext = doc.select(".atext").first();
-		return alltext.text();
-	}
-
+	// nothing to see
 }
